5764dbd7cd70fe280db005e0ccfe33e46b84090e
Febri Wardhana <44961442+Lukuzushiki@users.noreply.github.com>
2022-09-28 21:58:02 +0700

    feat: Initialize config database

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+  db: {
+    /* don't expose password or any sensitive info, done only for demo */
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "dev_test",
+  },
+};
+
+module.exports = config;
